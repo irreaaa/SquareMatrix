3235b70d4971c3fe80749e3b0d1805ddbed579df
Irina ྀིྀིྀིྀིྀིྀིྀ <146268256+irreaaa@users.noreply.github.com>
2024-10-23 16:00:50 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -1,103 +1,111 @@
-﻿Console.WriteLine("Задайте значение размерности квадратной матрицы: ");
-int n = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите минимальное значение случаных чисел: ");
-int min = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите максимальное значение случаных чисел: ");
-int max = Convert.ToInt32(Console.ReadLine());
-
-if (min > max)
+﻿using System;
+class Program
 {
-    Console.WriteLine("Максимальное значение должно быть больше минимального");
-    return;
-}
-
-int[,] matrix1 = new int[n, n];
-var random1 = new Random();
-int i;
-
-for (i = 0; i < n; i++)
-{
-    for (int j = 0; j < n; j++)
+    static void Main()
     {
-        matrix1[i, j] = random1.Next(min, max);
-    }
-}
 
-Console.WriteLine("Матрица А:");
-for (i = 0; i < n; i++)
-{
-    for (int j = 0; j < n; j++)
-    {
-        Console.Write(matrix1[i, j] + "\t");
-    }
-    Console.WriteLine(); 
-}
-Console.WriteLine();
+        Console.WriteLine("Задайте значение размерности квадратной матрицы: ");
+        int n = Convert.ToInt32(Console.ReadLine());
 
-int[,] matrix2 = new int[n, n];
-var random2 = new Random();
+        Console.WriteLine("Введите минимальное значение случайных чисел: ");
+        int min = Convert.ToInt32(Console.ReadLine());
 
-for (i = 0; i < n; i++)
-{
-    for (int j = 0; j < n; j++)
-    {
-        matrix2[i, j] = random2.Next(min, max);
-    }
-}
+        Console.WriteLine("Введите максимальное значение случайных чисел: ");
+        int max = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Матрица Б:");
-for (i = 0; i < n; i++)
-{
-        for (int j = 0; j < n; j++)
+        if (min > max)
         {
-        Console.Write(matrix2[i, j] + "\t");
+            Console.WriteLine("Максимальное значение должно быть больше минимального");
+            return;
         }
-    Console.WriteLine();
-}
-Console.WriteLine();
 
-//Сложение матриц
+        int[,] matrix1 = new int[n, n];
+        var random1 = new Random();
+        int i;
 
-int[,] matrix3 = new int[n, n];
-for (i  = 0; i  < n; i++)
-{
-    for (int j = 0;j < n; j++)
-    {
-        matrix3[i, j] = matrix1[i, j] + matrix2[i, j];
+        for (i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix1[i, j] = random1.Next(min, max);
+            }
+        }
+
+        Console.WriteLine("Матрица А:");
+        for (i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix1[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        int[,] matrix2 = new int[n, n];
+        var random2 = new Random();
+
+        for (i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix2[i, j] = random2.Next(min, max);
+            }
+        }
+
+        Console.WriteLine("Матрица Б:");
+        for (i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix2[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        //Сложение матриц
+
+        int[,] matrix3 = new int[n, n];
+        for (i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix3[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        }
+
+        Console.WriteLine("Сложение матриц А и Б"); for (i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix3[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        //Вычитание матриц
+
+        int[,] matrix4 = new int[n, n];
+        for (i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix4[i, j] = matrix1[i, j] - matrix2[i, j];
+            }
+        }
+
+        Console.WriteLine("Вычитание матрицы Б из матрицы А"); for (i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix4[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        Console.ReadKey();
     }
 }
-
-Console.WriteLine("Сложение матриц А и Б"); for (i = 0; i < n; i++)
-{
-    for (int j = 0; j < n; j++)
-    {
-        Console.Write(matrix3[i, j] + "\t");
-    }
-    Console.WriteLine();
-}
-Console.WriteLine();
-
-//Вычитание матриц
-
-int[,] matrix4 = new int[n, n];
-for (i = 0; i < n; i++)
-{
-    for (int j = 0; j < n; j++)
-    {
-        matrix4[i, j] = matrix1[i, j] - matrix2[i, j];
-    }
-}
-
-Console.WriteLine("Выичтание матрицы Б из матрицы А"); for (i = 0; i < n; i++)
-{
-    for (int j = 0; j < n; j++)
-    {
-        Console.Write(matrix4[i, j] + "\t");
-    }
-    Console.WriteLine();
-}
-Console.WriteLine();
-
-Console.ReadKey();
