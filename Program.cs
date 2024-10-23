7aff76af978bd3db3c01b1715b1d10c5269ea3ab
Irina ྀིྀིྀིྀིྀིྀིྀ <146268256+irreaaa@users.noreply.github.com>
2024-10-23 16:24:53 +0300

    third commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 class Program
 {
     static void Main()
@@ -19,93 +20,78 @@ class Program
             return;
         }
 
-        int[,] matrix1 = new int[n, n];
-        var random1 = new Random();
-        int i;
-
-        for (i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                matrix1[i, j] = random1.Next(min, max);
-            }
-        }
-
+        int[,] matrix1 = CreateMatrix(n, min, max);
         Console.WriteLine("Матрица А:");
-        for (i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                Console.Write(matrix1[i, j] + "\t");
-            }
-            Console.WriteLine();
-        }
-        Console.WriteLine();
+        ConsoleMatrix(matrix1, n);
 
-        int[,] matrix2 = new int[n, n];
-        var random2 = new Random();
-
-        for (i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                matrix2[i, j] = random2.Next(min, max);
-            }
-        }
-
+        int[,] matrix2 = CreateMatrix(n, min, max);
         Console.WriteLine("Матрица Б:");
-        for (i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                Console.Write(matrix2[i, j] + "\t");
-            }
-            Console.WriteLine();
-        }
-        Console.WriteLine();
+        ConsoleMatrix(matrix2, n);
 
-        //Сложение матриц
+        int[,] matrixSum = SummaMatrix(matrix1, matrix2, n);
+        Console.WriteLine("Сложение матриц А и Б:");
+        ConsoleMatrix(matrixSum, n);
 
-        int[,] matrix3 = new int[n, n];
-        for (i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                matrix3[i, j] = matrix1[i, j] + matrix2[i, j];
-            }
-        }
-
-        Console.WriteLine("Сложение матриц А и Б"); for (i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                Console.Write(matrix3[i, j] + "\t");
-            }
-            Console.WriteLine();
-        }
-        Console.WriteLine();
-
-        //Вычитание матриц
-
-        int[,] matrix4 = new int[n, n];
-        for (i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                matrix4[i, j] = matrix1[i, j] - matrix2[i, j];
-            }
-        }
-
-        Console.WriteLine("Вычитание матрицы Б из матрицы А"); for (i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                Console.Write(matrix4[i, j] + "\t");
-            }
-            Console.WriteLine();
-        }
-        Console.WriteLine();
+        int[,] matrixDifference = MinusMatrix(matrix1, matrix2, n);
+        Console.WriteLine("Вычитание матрицы Б из матрицы А:");
+        ConsoleMatrix(matrixDifference, n);
 
         Console.ReadKey();
     }
+
+    static int[,] CreateMatrix(int size, int min, int max)
+        {
+            int[,] matrix = new int[size, size];
+            var random = new Random();
+
+            for(int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = random.Next(min, max);
+                }
+            }
+            return matrix;
+        }
+
+        static void ConsoleMatrix(int[,] matrix, int size)
+        {
+            for(int i = 0;i < size; i++)
+            {
+                for( int j = 0; j < size; j++)
+                {
+                    Console.WriteLine(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static int[,] SummaMatrix(int[,] matrix1, int[,] matrix2, int size)
+        {
+            int[,] matrix3 = new int[size, size];
+            for(int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    matrix3[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+            return matrix3;
+        }
+
+        static int[,] MinusMatrix(int[,] matrix1, int[,] matrix2, int size)
+        {
+            int[,] matrix4 = new int[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0;j < size; j++)
+                {
+                    matrix4[i, j] = matrix1[i, j] - matrix2[i, j];
+                }
+            }
+            return matrix4;  
+        }
+    }
 }
+
